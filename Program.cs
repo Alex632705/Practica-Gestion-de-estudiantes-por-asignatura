b6f8a42571e5fdfda518cb7be5ed59ee6567ac4b
Alex José Rodríguez Taveras <alexrodrigueztaveras@gmail.com>
2026-02-10 21:58:43 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaGestionEstudiantes
+{
+     class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            SistemaGestion sistema = new SistemaGestion();
+            sistema.Ejecutar();
+        }
+        
+        
+    }
+}
